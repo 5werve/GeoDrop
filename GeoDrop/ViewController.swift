a20e2b419965ea7ac515f45ca3f4bf85da118807
Matthew <matthewvu@dhcp-10-250-104-25.harvard.edu>
2023-12-02 02:14:40 -0500

    commit message

---
+++ GeoDrop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeoDrop
+//
+//  Created by Matthew Vu on 12/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
